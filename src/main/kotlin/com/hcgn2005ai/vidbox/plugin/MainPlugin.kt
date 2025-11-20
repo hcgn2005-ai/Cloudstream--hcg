203679faf619bff6167a6f1f6984ec7fb456478e
hcgn2005-ai <hcgn2005@gmail.com>
2025-11-20 19:58:41 +0530

    Update MainPlugin.kt

--- src/main/kotlin/com/hcgn2005ai/vidbox/plugin/MainPlugin.kt
+++ src/main/kotlin/com/hcgn2005ai/vidbox/plugin/MainPlugin.kt
@@ -1,1 +1,13 @@
+package com.hcgn2005ai.vidbox.plugin
 
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class VidBoxPlugin: Plugin() {
+    override fun load(context: Context) {
+        // Register the main provider
+        registerMainAPI(VidBoxProvider())
+    }
+}
